4aba985a8d05a03d8926cb87eee1f1df94b84357
Ravirbm <96581286+Ravirbm@users.noreply.github.com>
2021-12-25 22:50:55 +0530

    Add files via upload
    Week1.day1

---
+++ Factorial.java
@@ -0,0 +1,13 @@
+package week1.day1;
+
+public class Factorial {
+	
+	public static void  main(String[] args) {
+	int total=1;
+			for (int r = 5; r>=1 ; r--) {
+				total = total * r;
+			}
+			System.out.println(total);
+	}
+
+}
