40aa704d5d4a40291b8cb1327d0b335317515f4b
Ravirbm <96581286+Ravirbm@users.noreply.github.com>
2022-01-12 20:16:03 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class Datatypes {
+	
+
+
+	public static void main(String[] args) {
+		//creating global variables with different data types
+		
+		String MobileModel = "Samsung";
+		int mobileWeight = 15;
+		boolean isFullyCharged = true;
+		double mobileCost = 5000.2030;
+		
+		//printing the output of all date types
+		System.out.println(MobileModel);
+		System.out.println(mobileWeight);
+		System.out.println(isFullyCharged);
+		System.out.println(mobileCost);
+	}
+
+}
--- OddNumbers.java
+++ OddNumbers.java
@@ -3,13 +3,22 @@ package week1.day1;
 public class OddNumbers {
 
 	public static void main(String[] args) {
-		for (int i = 1; i<=20; i++) {
-			if(i%2 != 0) {
-			System.out.println(i);
-			}
-		}
+		/*
+		 * Goal: Find the odd numbers in the specific range of numbers
+		 * 
+		 * Input: range of numbers (starting to end) [1 to 20]
+		 * Output: Printing only odd numbers [1,3,5,7,9...,19]
+		 * 
+		 * 
+		 */
 		
-			
+		//create for loop and use mod operator to separate odd numbers
+		int a;
+		for(int i = 1; i<=20; i++) {
+			a = i % 2;
+			if (a!=0)
+			System.out.println(i);
+		}
 
 	}
 
